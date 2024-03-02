92e435d538d248ee3ab9ddf76e509688c029c800
Fábio Correia <fabiodcorreia@gmail.com>
2024-03-02 16:00:46 +0000

    add top context with cancel to share

--- main.go
+++ main.go
@@ -7,11 +7,14 @@ import (
 	"net"
 	"os"
 
+	"github.com/fabiodcorreia/despensa-app/internal/handlers"
 	"github.com/fabiodcorreia/despensa-app/internal/routes"
 	"github.com/fabiodcorreia/despensa-app/internal/server"
+	"github.com/fabiodcorreia/despensa-app/internal/services"
 	"github.com/fabiodcorreia/despensa-app/internal/storage"
 )
 
+// default name placeholder
 var name = "app"
 
 // default version placeholder
@@ -24,18 +27,21 @@ var migrations embed.FS
 var public embed.FS
 
 func main() {
-	ctx := context.Background()
 	slog.Info("Starting application", "name", name, "version", version)
 
-	if err := run(ctx); err != nil {
-		slog.Error("App exited with error", err)
+	if err := run(); err != nil {
+		slog.Error("App exited with error", "error", err)
 		os.Exit(1)
 	}
 	slog.Info("App exited with success")
 }
 
-func run(ctx context.Context) error {
+func run() error {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
 	store, err := storage.NewStoreWithMigrations(
+		ctx,
 		server.GetDatabaseFile(),
 		migrations,
 	)
@@ -50,19 +56,21 @@ func run(ctx context.Context) error {
 	defer store.Disconnect()
 	slog.Info("Store connected", "database", server.GetDatabaseFile())
 
-	// https://github.com/samber/slog-echo/tree/main
-	// s := server.NewServer(ctx)
 	s := server.NewServer(
 		ctx,
 		server.WithSecure(),
 		server.WithRecover(),
 		server.WithRemoveTrailingSlash(),
 		server.WithLogger(),
+		// server.WithSession(),
 	)
+
 	s.AddPublic(public)
 
-	homeRoutes := routes.Home{}
-	s.AddRoutes(homeRoutes.Routes()...)
+	s.AddRoutes(routes.HomeRoute{})
+	s.AddRoutes(routes.NewLocationRoute(handlers.NewLocationHandler(services.NewLocationService(store))))
+	s.AddRoutes(routes.NewItemRoute(handlers.NewItemHandler(services.NewItemService(store))))
+	// s.AddRoutes(routes.SearchRoute{})
 
 	go s.Start(net.JoinHostPort(server.GetAddress(), server.GetPort()))
 	slog.Info("Server started", "address", server.GetAddress(), "port", server.GetPort())
