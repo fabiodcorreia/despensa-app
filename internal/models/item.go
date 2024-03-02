dce02b876b278caf86bfc718e93e377a021f80ae
Fábio Correia <fabiodcorreia@gmail.com>
2024-03-02 15:54:31 +0000

    add validation to the models

--- internal/models/item.go
+++ internal/models/item.go
@@ -1,19 +1,39 @@
 package models
 
+import "fmt"
+
 type Item struct {
-	Id         string
+	ID         string
 	Name       string
 	Quantity   int8
-	LocationId string
+	LocationID string
 }
 
-func NewItem(id, name string) Item {
+func NewItemWithID(id, name string) Item {
 	return Item{
-		Id:   id,
+		ID:   id,
 		Name: name,
 	}
 }
 
-func NewItemWitoutId(name string) Item {
-	return NewItem(NewShortID(name), name)
+func NewItem(name string) Item {
+	return NewItemWithID(NewShortID(name), name)
+}
+
+// TODO: move this errors to const
+
+func (i Item) Validate() error {
+	if i.ID == "" {
+		return fmt.Errorf("item id cannot be empty")
+	}
+	if i.Name == "" {
+		return fmt.Errorf("item name cannot be empty")
+	}
+	if i.LocationID == "" {
+		return fmt.Errorf("item location cannot be empty")
+	}
+	if i.Quantity < 0 {
+		return fmt.Errorf("item quantity cannot be negative")
+	}
+	return nil
 }
--- internal/models/location.go
+++ internal/models/location.go
@@ -1,13 +1,42 @@
 package models
 
+import (
+	"errors"
+)
+
 type Location struct {
-	Id   string
+	ID   string
 	Name string
 }
 
-func NewLocation(name string) Location {
+var ErrLocationIDTooLong = errors.New("location id cannot be longer than 10 characters")
+var ErrLocationIDEmpty = errors.New("location id cannot be empty")
+var ErrLocationNameEmpty = errors.New("location name cannot be empty")
+var ErrLocationNameTooLong = errors.New("location name cannot be longer than 24 characters")
+
+func NewLocationWithID(id, name string) Location {
 	return Location{
-		Id:   NewShortID(name),
+		ID:   id,
 		Name: name,
 	}
+}
+
+func NewLocation(name string) Location {
+	return NewLocationWithID(NewShortID(name), name)
+}
+
+func (l Location) Validate() error {
+	if l.ID == "" {
+		return ErrLocationIDEmpty
+	}
+	if l.Name == "" {
+		return ErrLocationNameEmpty
+	}
+	if len(l.ID) > 10 {
+		return ErrLocationIDTooLong
+	}
+	if len(l.Name) > 24 {
+		return ErrLocationNameTooLong
+	}
+	return nil
 }
